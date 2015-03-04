147bbc8f52b67225c9eccca865fc9067ebfa399d
Sean Clark Hess <seanhess@gmail.com>
2015-03-04 09:46:24 -0700

    super simple todo list, can't update yet

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,62 @@
 const React = window.React = require('react')
-const {Weeks} = require('./weeks')
-const {EntriesWeeks} = require('./store')
+const {map, append, partial, curry} = require('ramda')
 
-var App = React.createClass({
+var items = [{name: "one"}, {name: "two"}, {name: "three"}]
+
+const addItem = function(item) {
+  items = append(item, items)
+  render()
+}
+
+const updateItem = function(item) {
+  // need to identify it by id, then update it
+  // or by key path, or whatever.
+}
+
+const TodoItem = React.createClass({
   render() {
-    return <Weeks entries={EntriesWeeks}/>
+    const item = this.props.item
+
+    return <li>
+      <input value={item} />
+    </li>
   }
 })
 
-React.render(
-  <App/>,
-  document.getElementById('content')
-)
+const TodoList = React.createClass({
+  render() {
+    const items = this.props.items
+    const renderItem = i => <TodoItem item={i} />
 
+    return <ul>
+      {map(renderItem, items)}
+    </ul>
+  }
+})
+
+const TodoApp = React.createClass({
+  render() {
+    return <div>
+      <h1>Items</h1>
+      <TodoList items={this.props.items} />
+      <button onClick={partial(addItem, {name: "woot"})}>Add Item</button>
+    </div>
+  }
+})
+
+// I want to add items easily
+
+const App = React.createClass({
+  render() {
+    return <TodoApp items={items}/>
+  }
+})
+
+function render() {
+  React.render(
+    <App/>,
+    document.getElementById('content')
+  )
+}
+
+render()
