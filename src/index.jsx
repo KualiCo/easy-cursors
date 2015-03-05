63b5f4a60aea55dc4c89ccd83fcb6ce5ac767173
Sean Clark Hess <seanhess@gmail.com>
2015-03-05 12:24:11 -0700

    can read data from cursors

--- src/index.jsx
+++ src/index.jsx
@@ -1,55 +1,99 @@
 const React = window.React = require('react')
-const {map, append, partial, curry} = require('ramda')
+const {map, append, partial, curry, compose} = require('ramda')
+const shortid = require('shortid')
+const {projectile} = require('./projectile')
 
-var items = [{name: "one"}, {name: "two"}, {name: "three"}]
+//console.log("WOOO", projectile)
 
-const addItem = function(item) {
-  items = append(item, items)
-  render()
-}
 
-const updateItem = function(item) {
-  // need to identify it by id, then update it
-  // or by key path, or whatever.
-}
+// STATE --------------------------------------------------------
+
+// returns a state object, and automatically creates sub-cursors
+// state.sub('items') = cursor to items
+// state.sub('items').sub(0) = cursor to items[0]. 
+
+const state = projectile({
+  items: [
+    {id: 1, name: "one"}, 
+    {id: 2, name: "two"}, 
+    {id: 3, name: "three"}],
+})
+
+
+// state.cursor() returns current cursor
+
+// API: CURSOR
+  // cursor.value = x
+  // cursor.value
+  // cursor.update(func)  update value
+
+  // cursor.get(key)      child cursor
+  // cursor.get(keyPath)  child cursor
+
+  // cursor.toArray()     turn cursor to an array into array of cursors
+
 
 const TodoItem = React.createClass({
   render() {
-    const item = this.props.item
+
+    // BEST PRACTICE: unwrap the cursor immediately, don't use sub-cursors unless passing to another component
+    const cursor = this.props.cursor
+    const item = cursor.value
+
+    function onChange(e) {
+      cursor.update(setName(e.target.value))
+
+      // also works, but would voilate best practice. Don't play with cursors!
+      // write or update the entire cursor you own all at once
+      // item.sub('name').set(e.target.value)
+    }
+
+    // other ways of writing the same thing
+    // I'm not sure it's helpful to create change functions like this
+    // it's probably always clearer to define a callback above
+    // maybe: cursor.makeUpdate()
+    // const onChange2 = compose(cursor.update, setName, eventValue)
 
     return <li>
-      <input value={item} />
+      <input value={item.name} onChange={onChange}/>
     </li>
   }
 })
 
 const TodoList = React.createClass({
   render() {
-    const items = this.props.items
-    const renderItem = i => <TodoItem item={i} />
+    const cursor = this.props.cursor
 
+    // hmm, now this is challening
+    const renderItem = function(cursor) {
+      return <TodoItem cursor={cursor} key={cursor.value.id}/>
+    }
+
+    // can I make any cursor ACT like an array instead?
+    // I should try
     return <ul>
-      {map(renderItem, items)}
+      {map(renderItem, cursor.toArray())}
     </ul>
   }
 })
 
 const TodoApp = React.createClass({
   render() {
-    return <div>
+    const cursor = this.props.cursor
+    const onAdd = () => cursor.update(addEmptyItem)
+
+    return <div className="row small-12 columns">
       <h1>Items</h1>
-      <TodoList items={this.props.items} />
-      <button onClick={partial(addItem, {name: "woot"})}>Add Item</button>
+      <button onClick={onAdd}>Add Item</button>
+      <TodoList cursor={cursor} />
     </div>
   }
 })
 
 // I want to add items easily
-
 const App = React.createClass({
   render() {
-    return <TodoApp items={items}/>
-  }
+    return <TodoApp cursor={state.cursor().get('items')}/> }
 })
 
 function render() {
@@ -60,3 +104,32 @@ function render() {
 }
 
 render()
+state.onUpdate(render)
+
+
+
+
+// HELPERS --------------------------------------------------------
+// notice that none of these deal with cursors. They're all functions
+// that just deal with data, and are immutable-style
+
+const addItem = curry(function(item, items) {
+  return append(item, items)
+})
+
+const emptyItem = function() {
+  return {id: shortid.generate(), name: ""}
+}
+
+const addEmptyItem = compose(addItem, emptyItem)
+
+
+const setName = curry(function(value, item) {
+  item.name = value
+  return item
+})
+
+const eventValue = function(e) {
+  return e.target.value
+}
+
--- src/projectile.js
+++ src/projectile.js
@@ -1,1 +1,109 @@
+const {partial, append, range, curry, compose, map} = require("ramda")
+const {EventEmitter} = require("events")
 
+
+// API: CURSOR
+  // cursor.value = x
+  // cursor.value
+  // cursor.update(func)  update value
+
+  // cursor.get(key)      child cursor
+  // cursor.get(keyPath)  child cursor
+
+  // TODO try to make any cursor act enumerable? naw...
+  // cursor.toArray()     turn cursor to an array into array of cursors
+
+export function projectile(data) {
+  return new State(data)
+}
+
+export function cursor(data, keyPath) {
+  return new Cursor(data, keyPath)
+}
+
+export class State {
+
+  constructor(data) {
+    this._data = data
+    this._events = new EventEmitter()
+  }
+
+  onUpdate(f) {
+    this._events.on('update', f)
+  }
+
+  offUpdate(f) {
+    this._events.off('update', f)
+  }
+
+  cursor() {
+    return new Cursor(this._data)
+  }
+}
+
+
+export class Cursor {
+
+  constructor(data, keyPath = []) {
+    this._data = data
+    this._keyPath = keyPath
+  }
+
+  get(key) {
+    return childCursor(this._data, this._keyPath, key)
+  }
+
+  get value() {
+    return value(this)
+  }
+
+  set value(val) {
+    return this.update(() => val)
+  }
+
+  update(f) {
+    console.log("UPDATE", f)
+  }
+
+  toArray() {
+    const arrayCursors = compose(
+      map(childCursor(this._data, this._keyPath)),
+      indices
+    )
+    return arrayCursors(this.value)
+  }
+}
+
+//const toArray = compose(, indices)
+
+export const value = function(cursor) {
+  //console.log("VALUE", cursor)
+  return read(cursor._keyPath, cursor._data)
+}
+
+export const childCursor = curry(function(data, keyPath, key) {
+  //console.log("childCursor", data, keyPath, key)
+  return cursor(data, childKeyPath(key, keyPath))
+})
+
+function indices(arr) {
+  return range(0, arr.length)
+}
+
+function childKeyPath(key, parentKeyPath = []) {
+  return append(key, parentKeyPath)
+}
+
+function read(keyPath, data) {
+  //console.log("READ", data, keyPath)
+  if (!keyPath) return data
+
+  var asdf = keyPath.reduce(function(val, key) {
+    //console.log("get", key, val, val && val[key])
+    return val && val[key]
+  }, data)
+
+  //console.log("ASDF", asdf)
+
+  return asdf
+}
